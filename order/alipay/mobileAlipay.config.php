e6b589191e88a21dc355880657a929f317b99f81
mawei <mawei78951@126.com>
2014-12-04 13:20:51 +0800

    public_key
    public_key

--- order/alipay/mobileAlipay.config.php
+++ order/alipay/mobileAlipay.config.php
@@ -29,7 +29,7 @@ $alipay_config['partner'] = $INI['alipay']['mid'];;
 $alipay_config['private_key_path']	= 'key/rsa_private_key.pem';
 
 //支付宝公钥（后缀是.pen）文件相对路径
-$alipay_config['ali_public_key_path']= 'key/alipay_public_key.pem';
+$alipay_config['ali_public_key_path']= 'key/rsa_public_key.pem';
 
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
